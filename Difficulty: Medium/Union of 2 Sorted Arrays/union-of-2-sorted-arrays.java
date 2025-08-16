d2551fa839e144f58db535ad5f452ed5086df60a
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2025-08-16 22:35:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+        // code here
+        TreeSet<Integer>set=new TreeSet<>();
+        for(int i=0;i<a.length;i++)
+        {
+            set.add(a[i]);
+        }
+        
+        for(int i=0;i<b.length;i++)
+        {
+            set.add(b[i]);
+        }
+        
+        ArrayList<Integer>res=new ArrayList<>(set);
+        
+        return res;
+    }
+}
