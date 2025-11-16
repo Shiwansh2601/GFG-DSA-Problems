619e7445af6afdf498c9a1542243de082e3ff0f9
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2025-11-17 01:38:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Window Subsequence/minimum-window-subsequence.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String minWindow(String s1, String s2) {
+
+        int n = s1.length();
+        int m = s2.length();
+
+        int j = 0;
+        int minlen = n + 1;
+        String minwin = "";
+
+        for (int i = 0; i < n; i++) {
+
+            if (s1.charAt(i) == s2.charAt(j)) {
+                j++;
+            }
+
+            // full subsequence matched
+            if (j == m) {
+
+                int end = i + 1;
+                j--;
+
+    
+                int k = i;
+
+                while (j >= 0) {
+                    if (s1.charAt(k) == s2.charAt(j)) {
+                        j--;
+                    }
+                    k--;
+                }
+
+                k++;     
+                j = 0;   
+
+                if (end - k < minlen) {
+                    minlen = end - k;
+                    minwin = s1.substring(k, end);
+                }
+
+                // continue searching starting from k
+                i = k;
+            }
+        }
+
+        return minwin;
+    }
+}
