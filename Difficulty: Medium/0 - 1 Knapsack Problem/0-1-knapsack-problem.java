9b38e40b466b9a2f8add5bfd031288d2eafb94bb
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-01-29 01:44:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -3,29 +3,33 @@ class Solution {
     public int knapsack(int W, int val[], int wt[]) {
         // code here
         int[][] dp=new int[1001][1001];
-    for(int i=0;i<=1000;i++)
-    {
-        Arrays.fill(dp[i],-1);
-    }
+   
         return solve(W,val,wt,wt.length,dp);
+        
         
     }
     
      public int solve(int W, int val[], int wt[],int n,int[][] dp) {
         // code here
-        if(n==0||W==0)
-        return 0;
-        
-        if(dp[n][W]!=-1)
-        return dp[n][W];
-        
-        if(wt[n-1]<=W)
+        for(int i=1;i<=wt.length;i++)
         {
-            return dp[n][W]=Math.max(val[n-1]+solve(W-wt[n-1],val,wt,n-1,dp),solve(W,val,wt,n-1,dp));
+            for(int j=1;j<=W;j++)
+            {
+                
+              if(wt[i-1]<=j)
+             {
+                 dp[i][j]=Math.max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+             }
+            else
+           {
+             dp[i][j]=dp[i-1][j];
+           }
+           
+            }
         }
-        else
-        {
-            return dp[n][W]=solve(W,val,wt,n-1,dp);
-        }
+        
+         return dp[wt.length][W];
+        
+        
     }
 }
