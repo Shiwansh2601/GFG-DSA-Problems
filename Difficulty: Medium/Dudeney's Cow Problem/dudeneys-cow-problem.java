6d89f1b14ebaf89b2cf87db7be4e83bb77fa4285
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-04-24 08:54:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Dudeney's Cow Problem/dudeneys-cow-problem.java
@@ -0,0 +1,22 @@
+// User function Template for Java
+
+class Solution {
+    public int totalAnimal(int n) {
+        // code here
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        
+        return solve(n,dp);
+    }
+    
+    public int solve(int n,int[] dp)
+    {
+        
+        if(n==2 ||n==1)
+        return n;
+        
+        if(dp[n]!=-1)return dp[n];
+        
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    }
+}
