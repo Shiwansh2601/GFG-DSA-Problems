e12aac23b1bd41a9706ea204a5fa2088d1c72c7b
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-06-13 22:03:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Set all the bits in given range of a number/set-all-the-bits-in-given-range-of-a-number.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+
+class Solution {
+    static int setAllRangeBits(int N, int L, int R) {
+        // code here
+        R--;
+        L--;
+        int range=R-L+1;
+        int mask=((1<<range)-1)<<L;
+        
+        return N|mask;
+    }
+};
