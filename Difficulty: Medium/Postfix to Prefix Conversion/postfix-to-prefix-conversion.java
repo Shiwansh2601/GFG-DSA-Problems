f583cd6ef0548b6b87b48fe94fc3c24327fc4d27
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-07-13 17:08:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Postfix to Prefix Conversion/postfix-to-prefix-conversion.java
@@ -0,0 +1,59 @@
+class Solution {
+    static String postToPre(String exp) {
+        // code here
+        Stack<String>st=new Stack<>();
+        
+        int i=0;
+        while(i<exp.length())
+        {
+            char ch=exp.charAt(i);
+            if(ch=='+')
+            {
+                String s1=st.pop();
+                String s2=st.pop();
+                
+                String str="+"+s2+s1;
+                st.push(str);
+            }
+            else if(ch=='-')
+            {
+                String s1=st.pop();
+                String s2=st.pop();
+                
+              String str="-"+s2+s1;
+                st.push(str);
+            }
+            else if(ch=='*')
+            {
+                String s1=st.pop();
+                String s2=st.pop();
+                
+                String str="*"+s2+s1;
+                st.push(str);
+            }
+            else if(ch=='/')
+            {
+                String s1=st.pop();
+                String s2=st.pop();
+                
+               String str="/"+s2+s1;
+                st.push(str);
+            }
+            else if(ch=='^')
+            {
+                String s1=st.pop();
+                String s2=st.pop();
+                
+              String str="^"+s2+s1;
+                st.push(str);
+            }
+            else 
+            st.push(exp.substring(i,i+1));
+            
+            
+            i++;
+        }
+        
+        return st.pop();
+    }
+}
